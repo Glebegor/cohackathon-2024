c6218d73e48cf54d84aa21b485b8ab080abfb69c
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-12-06 20:13:39 +0100

    BE: Setuped interfaces and types.

---
+++ backend/MessagesService/src/usecases/messagesUsecase.ts
@@ -0,0 +1,12 @@
+
+
+
+interface IUsecaseMessages {
+    // createMessage()
+    // deleteMessage()
+    // updateMessage()
+    // getMessages()
+    // getLast5Messages()
+}
+
+export default IUsecaseMessages;
