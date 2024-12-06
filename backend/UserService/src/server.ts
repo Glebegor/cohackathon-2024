c68f12f382ab529afbc190143d30e5c14a46dff5
JirikPos <Jirikpos221@gmail.com>
2024-12-06 20:40:05 +0100

    BE:Renamed childHouse route, added routes to server.ts.

--- backend/UserService/src/server.ts
+++ backend/UserService/src/server.ts
@@ -1,6 +1,10 @@
 import express, { Express } from 'express';
 import cors from 'cors'
 
+import userRoutes from './routes/userRoutes'
+import userProfileRoutes from './routes/userProfileRoutes'
+import childHouseRoutes from './routes/userRoutes'
+
 const app: Express = express();
 const port: number = parseInt(process.env.PORT || '5002', 10);
 
@@ -8,7 +12,9 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/api/v2/reservations', );
+app.use('/api/v2/user', userRoutes);
+app.use('/api/v2/user-profile', userProfileRoutes);
+app.use('/api/v2/child-house', childHouseRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
