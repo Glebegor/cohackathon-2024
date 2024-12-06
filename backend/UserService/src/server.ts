fcbaf8ec9a57d6b489b9b5b3aa442d63bb9e1e0f
JirikPos <Jirikpos221@gmail.com>
2024-12-06 20:28:46 +0100

    BE:Updated routes.

--- backend/UserService/src/server.ts
+++ backend/UserService/src/server.ts
@@ -5,6 +5,10 @@ const app: Express = express();
 const port: number = parseInt(process.env.PORT || '5002', 10);
 
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/api/v2/reservations', );
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
