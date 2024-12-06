7f1b639a719b217913fe433a1ea66269c4392cf5
Adam Hojer <adam446643@gmail.com>
2024-12-06 15:17:15 +0100

    fe: imported css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import { Dashboard } from './pages/dashboard/dashboard'
 import { MainLayout } from './pages/layouts/main'
+import './index.css'
 
 function App() {
   return (
