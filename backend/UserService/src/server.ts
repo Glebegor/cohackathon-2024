b452e8a4a136fe25447c08daff0e71ee39fe86e9
JirikPos <Jirikpos221@gmail.com>
2024-12-07 00:59:26 +0100

    BE:Openned /diary api in backend.

--- backend/UserService/src/server.ts
+++ backend/UserService/src/server.ts
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use('/api/v2/diary', diaryRoutes);
 app.use('/api/v2/user', userRoutes);
 app.use('/api/v2/user-profile', userProfileRoutes);
 app.use('/api/v2/child-house', childHouseRoutes);
