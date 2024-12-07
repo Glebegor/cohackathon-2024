902a390fd8cfcf4f7451a0b00cf239b1da9a2f5e
Phi <honza.phi@gmail.com>
2024-12-07 15:19:55 +0100

    home

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,10 +1,13 @@
 import { Navbar } from "@/components/HomeNavbar"
+import background1 from "../../../public/mainBackground1.svg"
+import background2 from "../../../public/mainBackground2.svg"
 
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen">
+    <div className="flex items-start justify-start h-full w-full">
         <Navbar/>
-        <div>home</div>
+        <img src={background1} alt="mainImage" className="absolute top-20 right-0 w-1/4"/>
+        <img src={background2} alt="mainImage" className="absolute top-60 left-0 w-1/4"/>
     </div>
   )
 }
