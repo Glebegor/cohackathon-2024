efe6acdf51ff3e20d9437776682694485d4ef7b3
Adam Hojer <adam446643@gmail.com>
2024-12-07 13:34:40 +0100

    fe:layout general fixes

--- frontend/src/pages/diary/diary.tsx
+++ frontend/src/pages/diary/diary.tsx
@@ -86,32 +86,6 @@ const Diary:React.FC<DiaryProps> = () => {
 
     return (
         <div className="relative">
-            <div className="-z-50 top-0 left-0 absolute w-full">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 1440 320"
-                    preserveAspectRatio="none"
-                    style={{
-                    top: 0,
-                    left: 0,
-                    opacity: 1,
-                    width: "100%",
-                    height: "50%",
-                    zIndex: 1,
-                    }}
-                >
-                    <defs>
-                    <linearGradient id="wave-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
-                        <stop offset="0%" style={{ stopColor: "#e54f95", stopOpacity: 1 }} />
-                        <stop offset="100%" style={{ stopColor: "#4a328a", stopOpacity: 1 }} />
-                    </linearGradient>
-                    </defs>
-                    <path
-                    d="M0,64L30,69.3C60,75,120,85,180,101.3C240,117,300,139,360,133.3C420,128,480,96,540,90.7C600,85,660,107,720,122.7C780,139,840,149,900,133.3C960,117,1020,75,1080,74.7C1140,75,1200,117,1260,128C1320,139,1380,149,1410,154.7L1440,160V0H1410C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"
-                    fill="url(#wave-gradient)"
-                    />
-                </svg>
-            </div>
             <div className="flex flex-col gap-8 w-2/3 m-auto z-10 pt-14">
                 <div className="flex justify-between items-center gap-4">
                     <p className="text-4xl text-white font-sans font-semibold select-none">Můj deníček</p>
--- frontend/src/pages/layouts/main.tsx
+++ frontend/src/pages/layouts/main.tsx
@@ -7,34 +7,39 @@ import { Outlet } from "react-router";
 
 export const MainLayout: React.FC<MainLayoutProps> = () => {
     return (
-        <div className="relative">
-            <div style={{position: 'fixed', width: '100vw', height: '100vh', background: "#ffffff"}}>
-
-            </div>
-            {/*<Gradient
-                hexcolors={['4a328a', 'e54f95']}
-                variant="radial"
-                coords={[50, 50]} // Center position
-                timer={5000}
-                style={{
-                    position: 'fixed',
-                    width: '100vw',
-                    height: '100vh',
-                    opacity: .5,
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    zIndex: 0
-                }}
-            />*/}
-            <div className="z-10 min-h-screen relative w-screen">
-                <ContentLayout>
-                    <Navbar/>
-                    <div className="flex-1">
-                        <Outlet/>
+        <div className="z-10 min-h-screen relative w-full">
+            <ContentLayout>
+                <Navbar/>
+                <div className="flex-1 relative pb-8">
+                    <div className="-z-50 top-0 left-0 absolute w-full">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 1440 320"
+                            preserveAspectRatio="none"
+                            style={{
+                            top: 0,
+                            left: 0,
+                            opacity: 1,
+                            width: "100%",
+                            height: "50%",
+                            zIndex: 1,
+                            }}
+                        >
+                            <defs>
+                            <linearGradient id="wave-gradient" x1="0%" y1="0%" x2="100%" y2="100%">
+                                <stop offset="0%" style={{ stopColor: "#e54f95", stopOpacity: 1 }} />
+                                <stop offset="100%" style={{ stopColor: "#4a328a", stopOpacity: 1 }} />
+                            </linearGradient>
+                            </defs>
+                            <path
+                            d="M0,64L30,69.3C60,75,120,85,180,101.3C240,117,300,139,360,133.3C420,128,480,96,540,90.7C600,85,660,107,720,122.7C780,139,840,149,900,133.3C960,117,1020,75,1080,74.7C1140,75,1200,117,1260,128C1320,139,1380,149,1410,154.7L1440,160V0H1410C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"
+                            fill="url(#wave-gradient)"
+                            />
+                        </svg>
                     </div>
-                </ContentLayout>
-            </div>
+                    <Outlet/>
+                </div>
+            </ContentLayout>
         </div>
     );
 }
